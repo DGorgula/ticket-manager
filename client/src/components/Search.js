034fe991cf769e94a1f6f6da6679a3920aed79dc
DGorgula <gil@dankner.org>
2021-03-28 05:36:34 +0300

    done mark functionallity and display created

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Ticket from './Ticket.js';
 import '../styles/Search.css';
 
-function Search({ getLabelsElements, restoreHiddenTickets, hiddenTickets, tickets, setTickets, filterTickets, hideTicket, updateTicketLabels, labelObjectList, }) {
+function Search({ getLabelsElements, restoreHiddenTickets, hiddenTickets, tickets, setTickets, filterTickets, hideTicket, updateTicketLabels, labelObjectList, toggleDoneMark }) {
     const ticketElements = tickets.map((ticket, index) => {
         return (
-            <Ticket key={index} id={ticket._id || ticket.id} title={ticket.title} getLabelsElements={getLabelsElements} content={ticket.content} userEmail={ticket.userEmail} creationTime={ticket.creationTime} labels={ticket.labels} hideTicket={hideTicket} hiddenTickets={hiddenTickets} updateTicketLabels={updateTicketLabels} />
+            <Ticket key={index} id={ticket._id || ticket.id} title={ticket.title} done={ticket.done} getLabelsElements={getLabelsElements} content={ticket.content} userEmail={ticket.userEmail} creationTime={ticket.creationTime} labels={ticket.labels} hideTicket={hideTicket} hiddenTickets={hiddenTickets} updateTicketLabels={updateTicketLabels} toggleDoneMark={toggleDoneMark} />
         )
     });
     const showHiddenTicketsCounter = () => {
